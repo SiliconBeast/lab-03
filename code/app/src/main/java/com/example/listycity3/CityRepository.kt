16023285d5e25483faad855618027d75fe81ba84
suvir <suvir1@ualberta.ca>
2026-09-16 18:54:13 -0600

    add the reqd code snippets

--- code/app/src/main/java/com/example/listycity3/CityRepository.kt
+++ code/app/src/main/java/com/example/listycity3/CityRepository.kt
@@ -1,12 +1,15 @@
 package com.example.listycity3
-
+import androidx.compose.runtime.mutableStateListOf
 class CityRepository {
-    private val _cities = listOf(
+
+    private val _cities = mutableStateListOf(
         City("Edmonton", "AB"),
         City("Vancouver", "BC"),
-        City("Toronto", "ON")
-    )
+        City("Toronto", "ON"))
 
-    val cities: List<City>
-        get() = _cities
+        val cities: List<City>
+            get() = _cities
+        fun addCity(city: City)
+        { _cities.add(city)
+        }
 }
